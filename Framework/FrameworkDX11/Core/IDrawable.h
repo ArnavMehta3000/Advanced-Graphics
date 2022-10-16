ecd132a00b3787ba3f2d60061310b56df820d204
Arnav Mehta <arnavdatt@gmail.com>
2022-10-16 18:49:42 +0100

    Removed old framework

--- Framework/FrameworkDX11/Core/IDrawable.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-struct ID3D11Device;
-
-class IDrawable
-{
-public:
-	virtual void Draw() const noexcept = 0;
-};
--- Framework/FrameworkDX11/Core/Texture2D.cpp
+++
@@ -1,8 +0,0 @@
-#include "pch.h"
-#include "Texture2D.h"
-#include "Defines.h"
-
-void Texture2D::Release()
-{
-	COM_RELEASE(ResourceView);
-}
