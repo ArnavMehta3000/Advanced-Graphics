bda72b6a63bdb4c0168b3d73db2f94d92014e1ed
Arnav Mehta <arnavdatt@gmail.com>
2022-11-22 15:55:31 +0000

    Show textures in g buffer

--- AdvancedGraphics/AdvancedGraphics/Shaders/GBufferPS.hlsl
+++ AdvancedGraphics/AdvancedGraphics/Shaders/GBufferPS.hlsl
@@ -1,15 +1,26 @@
+#include "Structures.hlsli"
+
+Texture2D txDiffuse    : register(t0);
+Texture2D txNormal     : register(t1);
+Texture2D txHeight     : register(t2);
+SamplerState samLinear : register(s0);
+
+cbuffer MaterialProperties : register(b1)
+{
+    _Material Material;
+};
 
 struct VS_OUTPUT
 {
-    float4 Position : SV_POSITION;
+    float4 Position  : SV_POSITION;
     float4 PositionW : POSITION;
     float3 PositionT : POSITIONT;
-    float2 UV : TEXCOORD0;
-    float3 NormalT : NORMALT;
-    float3 NormalW : NORMALW;
+    float2 UV        : TEXCOORD0;
+    float3 NormalT   : NORMALT;
+    float3 NormalW   : NORMALW;
     float3 LightDirT : TLIGHTDIR;
-    float3 EyeDirT : TEYEDIR;
-    float3 EyePosT : EYEPOSITIONT;
+    float3 EyeDirT   : TEYEDIR;
+    float3 EyePosT   : EYEPOSITIONT;
 };
 
 struct PS_OUTPUT
@@ -23,9 +34,23 @@ struct PS_OUTPUT
 PS_OUTPUT PS(VS_OUTPUT input)
 {
     PS_OUTPUT output = (PS_OUTPUT) 0;
+
+    [branch]
+    if (Material.UseTexture)
+        output.Diffuse = txDiffuse.Sample(samLinear, input.UV);
+    else
+        output.Diffuse = Material.Diffuse;  // White color 
     
-    output.Diffuse = float4(1, 1, 1, 1);
-    output.Normal  = float4(input.NormalW.xyz, 1.0f);
+    [branch]
+    if (Material.UseNormals)
+    {
+        float4 texNormal   = txNormal.Sample(samLinear, input.UV);
+        float4 bumpNormalT = float4(normalize(2.0f * texNormal.xyz - 1.0f).xyz, 1.0f);
+        output.Normal      = bumpNormalT;
+    }
+    else
+        output.Normal = float4(input.NormalT.xyz, 1.0f);
+        
     output.Position = input.PositionW;
     
     return output;
